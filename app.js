f8541ff2f9eb4ba8e14763a300305bce03ab4b66
kevin mijares <kevmdm@gmail.com>
2022-02-25 19:37:01 -0400

    Obtenemos UTXO en prueba

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const express = require('express');
 
-
-
-
 const app = express();
 
 app.set('view engine', 'ejs');
--- public/webApp.js
+++ public/webApp.js
@@ -1,4 +1,4 @@
-import * as wasm from "https://cdn.jsdelivr.net/npm/@emurgo/cardano-serialization-lib-asmjs@9.1.2/cardano_serialization_lib.min.js";
+import * as wasm from "https://cdn.jsdelivr.net/npm/@emurgo/cardano-serialization-lib-asmjs@10.0.4/cardano_serialization_lib.js";
 
 function hexToBytes(hex) {
   for (var bytes = [], c = 0; c < hex.length; c += 2)
@@ -6,12 +6,15 @@ function hexToBytes(hex) {
   return bytes;
 }
 
+
 function nami() {
   console.log("Funcion corriendo");
   window.cardano.nami.enable().then((api) => {//asi se usa una promesa
     console.log("Conecte con cardano");
     console.log(api);
     api.getBalance().then((res) => {
+      console.log('opten respuesta de balance')
+      console.log(res)
       const balance = wasm.Value.from_bytes(hexToBytes(res));
       const lovelaces = balance.coin().to_str();
 
@@ -19,6 +22,16 @@ function nami() {
       const adaValue = lovelaces / 1000000;
       console.log('Kev Tiene ' + adaValue + ' Adas');
       alert('Kev Tiene ' + adaValue + ' Adas');
+    });
+
+    //opten UTXO sin serializar 
+
+    api.getUtxos().then((res) => {
+      console.log('getting utxo');
+      const utxo = res[0];
+      console.log(utxo);
+     
+
     });
   });
 }
