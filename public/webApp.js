1382d1c484dd80933a1232b25d26fef8631b1f53
kevin mijares <kevmdm@gmail.com>
2022-02-28 13:00:37 -0400

    mejoras a como se ve el codigo !!!

--- public/webApp.js
+++ public/webApp.js
@@ -1,5 +1,6 @@
 import * as wasm from "https://cdn.jsdelivr.net/npm/@emurgo/cardano-serialization-lib-asmjs@10.0.4/cardano_serialization_lib.js";
 
+
 function hexToBytes(hex) {
   for (var bytes = [], c = 0; c < hex.length; c += 2)
     bytes.push(parseInt(hex.substr(c, 2), 16));
@@ -7,22 +8,22 @@ function hexToBytes(hex) {
 }
 
 
-await cardano.nami.enable().then((api) => {
+const api = await cardano.nami.enable();
+console.log(api);
+console.log('new balance')
 
-  api.getBalance().then(res => {
-    console.log(res); //respuesta de la instruccion en bytes 
-    const balance = wasm.Value.from_bytes(hexToBytes(res));
-    const lovelaces = balance.coin().to_str();
-    console.log(lovelaces + ' Lovelace');
-    const adaValue = lovelaces / 1000000;
-    console.log(adaValue + ' Adas');
-  });
+const getBalance = await api.getBalance();
 
-  api.getUtxos().then(res => {
-    console.log('getting utxo');
-    const utxoCbor = res[0];
-    console.log(utxoCbor);
-  });
+const balance =  wasm.Value.from_bytes(hexToBytes(getBalance));
+const lovelaces = balance.coin().to_str();
+const adaValue = lovelaces / 1000000;
+console.log(adaValue + ' Adas');
 
-});
+const adress = (await api.getUsedAddresses())[0];
+
+console.log(adress);
+
+
+
+
 
